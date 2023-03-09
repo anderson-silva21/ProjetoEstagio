188bf19e023cbba1dad1f3a3765daa48d42f5393
Anderson Soares da Silva <980166@queroquero.local>
2023-03-08 22:52:50 -0300

    finish solicitFerias

--- qqferias/src/pages/SolicitacaoFerias/SolicitacaoFerias.js
+++ qqferias/src/pages/SolicitacaoFerias/SolicitacaoFerias.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
-import moment from 'moment';
+import React, { useState, useEffect } from 'react';
+import { Calendar, momentLocalizer, Year } from 'react-big-calendar';
+import { Month } from 'react-big-calendar';
+import moment, { months } from 'moment';
 import 'moment/locale/pt-br';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import './SolicitacaoFerias.css'
@@ -16,11 +17,40 @@ function MyCalendar(){
 
     const [events, setEvents] = useState();
 
-    const handleSearch = (query) => {
-      // fazer a busca no banco de dados ou API aqui
-      // atualizar o estado com os resultados da pesquisa
-      setSearchResults([]);
+    const [selectedMonth, setSelectedMonth] = useState('a definir');
+
+    const [selectedDays, setSelectedDays] = useState('');
+
+    const [selectedOption, setSelectedOption] = useState('');
+
+    const [endDate, setEndDate] = useState('a definir');
+
+    const handleSelectChange = (event) => {
+        setSelectedOption(event.target.value);
     };
+
+    const handleMonthChange = (event) => {
+        setSelectedMonth(event.target.value);
+    };
+
+    const handleDayChange = (event) => {
+        setSelectedDays(event.target.value);
+    }
+
+    const handleSearch = (query) => {
+        // fazer a busca no banco de dados ou API aqui
+        // atualizar o estado com os resultados da pesquisa
+        setSearchResults([]);
+    };
+    useEffect(() => {
+        if (selectedDays && selectedOption) {
+          const start = moment(`${selectedOption} ${selectedMonth}`, 'D MMMM');
+          const end = start.clone().add(selectedDays, 'days');
+          setEndDate(end.format('DD/MM/YYYY'));
+        } else {
+          setEndDate('a definir');
+        }
+      }, [selectedDays, selectedOption, selectedMonth]);
     return (
         <div>
             <SearchBar onSearch={handleSearch} />
@@ -41,7 +71,7 @@ function MyCalendar(){
                         
                         <div className="input-container">
                             <label className="nome-login">Mês</label>
-                            <select className="input-login-selector">
+                            <select className="input-solicit-selector" value={selectedMonth} onChange={handleMonthChange}>
                                 <option value="Janeiro">Janeiro</option>
                                 <option value="Fevereiro">Fevereiro</option>
                                 <option value="Marco">Março</option>
@@ -59,15 +89,15 @@ function MyCalendar(){
                         <div className="input-container-radio">
                             <label className="nome-login">Quantidade de dias</label>
                             <div className="radio-btn">
-                                <input type="radio" name="opcoes1" value="05"></input>
+                                <input type="radio" name="opcoes1" value="05" checked={selectedDays === '5'} onChange={handleDayChange}></input>
                                 <label>05</label>
-                                <input type="radio" name="opcoes1" value="10"></input>
+                                <input type="radio" name="opcoes1" value="10" checked={selectedDays === '10'} onChange={handleDayChange}></input>
                                 <label>10</label>
-                                <input type="radio" name="opcoes1" value="15"></input>
+                                <input type="radio" name="opcoes1" value="15" checked={selectedDays === '15'} onChange={handleDayChange}></input>
                                 <label>15</label>
-                                <input type="radio" name="opcoes1" value="20"></input>
+                                <input type="radio" name="opcoes1" value="20" checked={selectedDays === '20'} onChange={handleDayChange}></input>
                                 <label>20</label>
-                                <input type="radio" name="opcoes1" value="30"></input>
+                                <input type="radio" name="opcoes1" value="30" checked={selectedDays === '30'} onChange={handleDayChange}></input>
                                 <label>30</label>
                             </div>                  
                         </div>
@@ -75,7 +105,22 @@ function MyCalendar(){
                             <div className="radio-btn">
                                 <input  type="radio" name="opcoes" value="decimo-terceiro"></input>
                                 <label>Adiantamento do 13º</label>
-                                
+                            </div>
+                        </div>
+                        <div className='input-container-begin'>
+                            <label className= 'nome-login'>Inicio</label>
+                            <select className='input-solicit-selector' onChange={handleSelectChange}>
+                                {Array.from({ length: moment().daysInMonth() }, (_, i) => i + 1)
+                                .map(day => (
+                                    <option key={day} value={day}>
+                                    {day} de {selectedMonth}
+                                    </option>
+                                ))}
+                            </select>
+                            
+                            <div className='end-selector'>
+                                <label className= 'nome-login'>Fim</label>
+                                <label className='end-date-selector'>{endDate}</label>
                             </div>
                         </div>
                     <button className="solicit-button" type="submit">Enviar solicitacao para o gestor</button>
